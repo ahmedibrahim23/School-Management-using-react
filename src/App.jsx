dab29a2bca82a15c1208601893c3bd707baba835
ahmedibrahim23 <axmedaadan484@gmail.com>
2024-07-30 16:23:15 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,40 +14,44 @@ import StudentForm from './components/student/StudentForm';
 import StudentDetail from './components/student/StudentDetail';
 import EditStudentForm from './components/student/EditStudentForm';
 import TeacherSidebar from './components/sidebar/TeacherSidebar';
+import Admin from './dashBoards/Admin';
 
 
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Router>
-        <Navbar />   
-        <div className="flex flex-grow">
-          {/* <TeacherSidebar/> */}
-
-         <Sidebar />
-        <div className="flex-1 p-4 transition-all duration-300">
-          <Routes>
-            <Route path="/" element={<div>Dashboard overviews</div>}/>
-            <Route path="/teachers" element={<TeacherList />} />
-            <Route path="/teachers/new" element={<TeacherForm />} />
-            {<Route path="/details/:id" element={<TeacherDetail />} /> }
-            <Route path="/editteacher/:id" element={<EditTeacherForm />} />
-            {/* Add routes for Student, Class, Subject, Reports, etc. */}
-            <Route path="/students" element={<StudentList/>} />
-            <Route path="/students/new" element={<StudentForm/>}/>
-            <Route path='/stdDetails/:id' element={<StudentDetail/>}/>
-            <Route path='/editstudent/:id' element={<EditStudentForm/>}/>
-            <Route path="/classes" element={<ClassList/>} />
-            <Route path="/classes/new" element={<ClassForm/>} />
-            <Route path="/subjects" element={<SubjectList/>} />
-            <Route path="/reports" element={<div>Reports Component</div>} />
-            <Route path="/logout" element={<div>Logout Component</div>} />
-          </Routes>
-        </div>
-        </div>
-      </Router>
+    <div>
+      <Admin/>
     </div>
+    // <div className="flex flex-col min-h-screen">
+    //   <Router>
+    //     <Navbar />   
+    //     <div className="flex flex-grow">
+    //       {/* <TeacherSidebar/> */}
+
+    //      <Sidebar />
+    //     <div className="flex-1 p-4 transition-all duration-300">
+    //       <Routes>
+    //         <Route path="/" element={<div>Dashboard overviews</div>}/>
+    //         <Route path="/teachers" element={<TeacherList />} />
+    //         <Route path="/teachers/new" element={<TeacherForm />} />
+    //         {<Route path="/details/:id" element={<TeacherDetail />} /> }
+    //         <Route path="/editteacher/:id" element={<EditTeacherForm />} />
+    //         {/* Add routes for Student, Class, Subject, Reports, etc. */}
+    //         <Route path="/students" element={<StudentList/>} />
+    //         <Route path="/students/new" element={<StudentForm/>}/>
+    //         <Route path='/stdDetails/:id' element={<StudentDetail/>}/>
+    //         <Route path='/editstudent/:id' element={<EditStudentForm/>}/>
+    //         <Route path="/classes" element={<ClassList/>} />
+    //         <Route path="/classes/new" element={<ClassForm/>} />
+    //         <Route path="/subjects" element={<SubjectList/>} />
+    //         <Route path="/reports" element={<div>Reports Component</div>} />
+    //         <Route path="/logout" element={<div>Logout Component</div>} />
+    //       </Routes>
+    //     </div>
+    //     </div>
+    //   </Router>
+    // </div>
   )
 }
 
--- src/dashBoards/Admin.jsx
+++ src/dashBoards/Admin.jsx
@@ -3,6 +3,9 @@ import Sidebar from '../components/sidebar/Sidebar'
 import { Route, Router, Routes } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar'
 import TeacherForm from '../components/teacher/TeacherForm'
+import TeacherList from '../components/teacher/TeacherList'
+import TeacherDetail from '../components/teacher/TeacherDatail'
+import EditTeacherForm from '../components/teacher/EditTeacherForm'
 
 
 const Admin = () => {
@@ -12,8 +15,11 @@ const Admin = () => {
       <Sidebar/>
       <Routes>
         <Route path='/' element={<Navbar/>}/>
-        <Route path='/teachers' element={<TeacherForm/>}/>
-        
+        <Route path='/teachers' element={<TeacherList/>}/>
+        <Route path='/teachers/new' element={<TeacherForm/>}/>
+        <Route path='/details/:id' element={<TeacherDetail/>}/>
+        <Route path='/details/:id' element={<TeacherDetail/>}/>
+        <Route path="/editteacher/:id" element={<EditTeacherForm />} />
       </Routes>
     </Router>
   </div>
