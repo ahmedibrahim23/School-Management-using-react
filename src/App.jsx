47909df97134d6bf5352164cabdef66b7eb4c612
shukri muuse <shukrimuuse145@gmail.com>
2024-07-30 21:36:25 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import TeacherDashboard from './dashBoards/TeacherDashboard'
-// import Admin from './dashBoards/Admin';
+import Admin from './dashBoards/Admin';
 // import LoginPage from './components/login page/LoginPage';
 
 const App = () => {
   return (
     <div>
-<
-      { <TeacherDashboard/> }
+       {/* {<TeacherDashboard/> }  */}
       {/* <LoginPage/> */}
-      {/* <Admin/> */}
-
+       {<Admin/>} 
     </div>
   )
 }
